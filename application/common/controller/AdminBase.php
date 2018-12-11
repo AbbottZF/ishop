4f3c60614445c5910b47daba69a5bf6c7f5307ef
AbbottZF <39108584+AbbottZF@users.noreply.github.com>
2018-12-11 23:45:39 +0800

    新建model

---
+++ application/common/controller/AdminBase.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\common\controller;
+use think\Controller;
+
+class AdminBase extends Controller{
+    
+    protected function _initialize() {
+        parent::_initialize();
+    }
+}
