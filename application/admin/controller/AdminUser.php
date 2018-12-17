b1a9c426c0d94836b95fe9b0768023c1199dc142
AbbottZF <39108584+AbbottZF@users.noreply.github.com>
2018-12-17 22:50:31 +0800

    用户登录

---
+++ application/admin/controller/AdminUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\admin\controller;
+use app\common\controller\AdminBase;
+use app\common\model\AdminUser as AU;
+use think\Config;
+
+class AdminUser extends AdminBase{
+    
+    protected $admin_user_model;
+
+    protected function _initialize() {
+        parent::_initialize();
+        $this->admin_user_model = new AU();
+    }
+    /**
+     * 登陆
+     * @return type
+     */
+    public function login(){
+        if($this->request->isPost()){
+            $data = $this->request->param();
+            $where = [
+                'name'=>$data['name'],
+                'password'=> md5($data['password'].Config::get('salf')),
+                'status'=>1
+            ];
+            $info = $this->admin_user_model->getInfo($where);
+            return adminMsg($info);
+        }
+    }
+}
