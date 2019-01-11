3394dfea354edf49d4d5ec3cd78500f8b099c1c2
AbbottZF <39108584+AbbottZF@users.noreply.github.com>
2019-01-11 23:38:41 +0800

    19.0111.1

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\admin\controller;
+use app\common\controller\AdminBase;
+/**
+ * Description of Index
+ *
+ * @author zfeng
+ */
+class Index extends AdminBase{
+    
+    public function index(){
+        return $this->fetch();
+    }
+}
